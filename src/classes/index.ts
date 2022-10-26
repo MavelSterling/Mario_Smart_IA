b0c96f4f66cba6425ec308e88e1ffb6b08c9ebe4
juandsoto <juandsoto1006@gmail.com>
2022-10-25 21:30:39 -0500

    feature: implemented searchAlgorithms class

--- src/classes/index.ts
+++ src/classes/index.ts
@@ -3,4 +3,5 @@ export { default as Coordinate } from "./Coordinate";
 export { default as Mario } from "./Mario";
 export { default as Matrix } from "./Matrix";
 export { default as Node } from "./Node";
+export { default as SearchAlgorithms } from "./SearchAlgorithms";
 export { default as Solution } from "./Solution";
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { Animation, Coordinate, Mario, Matrix, Node, Solution } from "./classes";
+import { Animation, Mario, Matrix, Node, Solution, SearchAlgorithms } from "./classes";
 import { AnimationSetup } from "./classes/Animation";
 import "./style.css";
 
 let MATRIX: string;
 let INTERVAL_ID: number | undefined;
-let bfs: Node[] = [];
+let nodeList: Node[] = [];
 
 document.getElementById("inputFile")!.addEventListener("change", function () {
   var fr = new FileReader();
@@ -12,7 +12,6 @@ document.getElementById("inputFile")!.addEventListener("change", function () {
     reloadGame(false);
     new Matrix(fr.result as string);
     MATRIX = fr.result as string;
-    main();
   };
   //@ts-ignore
   fr.readAsText(this.files[0]);
@@ -29,7 +28,7 @@ const resetGame = () => {
   clearInterval(INTERVAL_ID);
   Mario.reset();
   Solution.reset();
-  bfs = [];
+  nodeList = [];
   initAnimationButton.removeAttribute("disabled");
 };
 
@@ -38,10 +37,9 @@ const reloadGame = (sameGame: boolean = true): void => {
   resetGame();
   if (!sameGame) return;
   new Matrix(MATRIX);
-  main();
 };
 
-const startAnimation = (e: SubmitEvent) => {
+const startGame = (e: SubmitEvent) => {
   e.preventDefault();
   if (Matrix.isEmpty()) return;
   const data: AnimationSetup = Object.fromEntries(new FormData(initAnimationForm) as any) as AnimationSetup;
@@ -50,75 +48,22 @@ const startAnimation = (e: SubmitEvent) => {
     ...data,
   });
   initAnimationButton.toggleAttribute("disabled");
-  animate();
+  main();
 };
 
 toggleSidebarButton.addEventListener("click", toggleSidebar);
-initAnimationForm.addEventListener("submit", startAnimation);
+initAnimationForm.addEventListener("submit", startGame);
 reloadButton.addEventListener("click", () => reloadGame());
 
-const MAIN_GAME =
-  "1 0 0 0 0 0 0 0 1 1\r\n0 3 1 1 0 1 1 0 0 1\r\n1 1 1 1 0 1 1 1 3 0\r\n0 0 0 0 0 1 1 1 1 0\r\n2 1 1 1 0 0 0 0 5 5\r\n0 0 0 1 0 1 1 1 1 5\r\n0 1 0 0 0 5 5 5 0 0\r\n0 1 1 0 1 1 1 1 1 0\r\n0 4 4 0 1 1 1 6 0 0\r\n1 1 1 1 1 1 1 0 1 1";
+// const MAIN_GAME = "1 0 0 0 0 0 0 0 1 1\r\n0 3 1 1 0 1 1 0 0 1\r\n1 1 1 1 0 1 1 1 3 0\r\n0 0 0 0 0 1 1 1 1 0\r\n2 1 1 1 0 0 0 0 5 5\r\n0 0 0 1 0 1 1 1 1 5\r\n0 1 0 0 0 5 5 5 0 0\r\n0 1 1 0 1 1 1 1 1 0\r\n0 4 4 0 1 1 1 6 0 0\r\n1 1 1 1 1 1 1 0 1 1";
 
 // new Matrix(MAIN_GAME);
 // main();
 
 function main() {
-  new Mario(Matrix.findPlayer());
+  SearchAlgorithms.breadthFirstSearch(nodeList);
 
-  let currentNode: Node = new Node(null, Mario.position, Matrix.matrix);
-
-  bfs.push(currentNode);
-  while (bfs.length && !currentNode.isPrincess()) {
-    bfs.shift();
-
-    if (!currentNode.isWall()) {
-      //GO LEFT
-      if (currentNode.position.y > 0) {
-        const newPosition = new Coordinate(currentNode.position.x, currentNode.position.y - 1);
-        if (!bfs.find(node => node.position.x === newPosition.x && node.position.y === newPosition.y)) {
-          bfs.push(new Node(currentNode, newPosition, Matrix.matrix));
-        }
-      }
-      //GO RIGHT
-      if (currentNode.position.y < Matrix.matrix[0].length - 1) {
-        const newPosition = new Coordinate(currentNode.position.x, currentNode.position.y + 1);
-        if (!bfs.find(node => node.position.x === newPosition.x && node.position.y === newPosition.y)) {
-          bfs.push(new Node(currentNode, newPosition, Matrix.matrix));
-        }
-      }
-      //GO UP
-      if (currentNode.position.x > 0) {
-        const newPosition = new Coordinate(currentNode.position.x - 1, currentNode.position.y);
-        if (!bfs.find(node => node.position.x === newPosition.x && node.position.y === newPosition.y)) {
-          bfs.push(new Node(currentNode, newPosition, Matrix.matrix));
-        }
-      }
-      //GO DOWN
-      if (currentNode.position.x < Matrix.matrix.length - 1) {
-        const newPosition = new Coordinate(currentNode.position.x + 1, currentNode.position.y);
-        if (!bfs.find(node => node.position.x === newPosition.x && node.position.y === newPosition.y)) {
-          bfs.push(new Node(currentNode, newPosition, Matrix.matrix));
-        }
-      }
-    }
-    currentNode = bfs[0];
-  }
-
-  Solution.solution = bfs[0].path;
-  Solution.solution.forEach(Solution.buildNodeCost);
-  Solution.buildTotalCost();
-
-  console.log("--SOLUTION--");
-  for (let i = 0; i < Solution.solution.length; i++) {
-    console.log(
-      `(${Solution.solution[i].position.x},${Solution.solution[i].position.y}) - cost: ${Solution.solution[i].cost} - object: ${Solution.solution[i].object}`
-    );
-  }
-  console.log("STATS");
-  console.log("Total cost:", Solution.cost);
-
-  Solution.solution.shift();
+  animate();
 }
 
 function animate(): void {
